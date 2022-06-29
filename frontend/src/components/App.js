7fa6b574ba339171192ecc9912c9ab7a1b46f2a2
Ashsenior <ashutoshjunior@gmail.com>
2022-06-30 00:47:53 +0530

    few changes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,20 +2,12 @@ import React from 'react';
 import {render} from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './Home/HomePage';
-import MemberForm from './Member/Member';
-import UniversityForm from './University/UniversityForm';
-import Universities from './University/Universities';
-import University from './University/University';
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<HomePage />} />
-                <Route path='/university-form' element={<UniversityForm />} />
-                <Route path='/member-form' element={<MemberForm />} />
-                <Route path='/universities' element={<Universities />} />
-                <Route path='/university/:id' element={<University />} />
             </Routes>
         </Router>
     );
--- frontend/urls.py
+++ frontend/urls.py
@@ -7,9 +7,5 @@ app_name = "frontend"
 
 urlpatterns = [
     path('',index),
-    path('university-form/',index),
-    path('member-form/',index),
-    path('universities/',index),
-    path('university/<str:id>',index),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

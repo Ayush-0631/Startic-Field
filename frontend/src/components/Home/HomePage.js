fa2f89416a1bfa5e9707574b796a3e72fb7a7a38
Ashsenior <ashutoshjunior@gmail.com>
2022-07-02 20:44:29 +0530

    working now

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -5,7 +5,7 @@ export default function HomePage() {
     return (
         <div className='w3-margin w3-padding' >
             <h1 className='w3-text-blue' >This Webapp is running UwU !</h1>
-            <h2 className='uwu' >Now u can START working ;)</h2>
+            <h2 className='uwu' >Now u can START working ;</h2>
         </div>
     );
 }

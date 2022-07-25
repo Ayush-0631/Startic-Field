4da17ea6d8053ac53c1ca4e8b51c05696caea4e6
Anirudh <anirudhmukkamala@gamil.com>
2022-07-25 22:19:46 +0530

    templates are integrated

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # paths will be declared here 
+    path('',Home),
+    path('/about',About),
+    path('/contact',Contact),
+    path('/programs',Programs),
+    path('/reverse-pitch',Reverseptich),
+    path('/sign-up',Signup),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/views.py
+++ backend/views.py
@@ -10,3 +10,26 @@ def enroll_in_event(request, code):
     profile = Profile.objects.get(user=request.user)
     add_event_to_calendar(profile=profile, event=event)
     UserEventMap.objects.create(event=event, user=profile)
+
+def Home(request):
+    return render(request,"index.html")
+
+
+def Signup(request):
+    return render(request,"sign-up.html")
+
+
+def Programs(request):
+    return render(request,"Programs-page.html")
+
+
+def Reverseptich(request):
+    return render(request,"reverse-pitch.html")
+
+
+def About(request):
+    return render(request,"about-us.html")
+
+
+def Contact(request):
+    return render(request,"contact.html")

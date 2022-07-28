f1a52b3e0105bd72fb1b4ef415570c9288894d21
Ashsenior <ashutoshjunior@gmail.com>
2022-07-28 19:55:52 +0530

    login signup complete

--- backend/urls.py
+++ backend/urls.py
@@ -5,14 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # paths will be declared here 
-    path('',Home),
+    path('',Home, name='home'),
     path('about',About),
     path('contact',Contact),
     path('programs',Programs),
     path('reverse-pitch',Reverseptich),
-    path('sign-up',Signup),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('events',Events),
-    path('sign',Signup),
+    path('sign',Signup, name='sign-up'),
     path('form1',FormsStudent),
     path('form3',FormsStartup),
     path('form2',Formsreversepitch),
--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import *
 from apis.views.calendar import *
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 # Create your views here.
 
 
@@ -39,7 +41,72 @@ def Events(request):
     return render(request,"events_page.html")
 
 def Signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 6:
+                    messages.error(request, 'Password is too short')
+                    return render(request, 'register.html')
+
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.is_active = True
+                user.save()
+                return redirect('home')
+
+            messages.warning(request, "This Email already exists!")
+            return render(request, 'register.html', context)
+        else:
+            messages.warning(request, "This username already exists!")
+            return render(request, 'register.html', context)
+
     return render(request,"LoginPage.html")
+
+def login(request):
+        username = request.POST['username']
+        password = request.POST['password']
+        if username and password:
+            if User.objects.filter(username=username).exists():
+                user = auth.authenticate(
+                    username=username, password=password)
+                if user:
+                    if user.is_active:
+                        auth.login(request, user)
+                        messages.success(request,"loggedin succesfully")
+                        return redirect("home")
+
+                    messages.error(
+                        request, "Account is not active,please check your email"
+                    )
+
+            elif User.objects.filter(email=username).exists():
+                user = User.objects.get(email=username)
+                user = auth.authenticate(
+                    username=user.username, password=password)
+                if user:
+                    if user.is_active:
+                        auth.login(request, user)
+                        messages.success(request,"loggedin succesfully")
+                        return redirect("home")
+
+                    messages.error(
+                        request, "Account is not active,please check your email"
+                    )
+            elif (User.objects.filter(email=username).exists() or User.objects.filter(username=username).exists() == False):
+                messages.warning(
+                    request, "The username or Email you have entered does not exist.")
+                return render(request, 'login.html')
+
+        messages.warning(request, 'Invalid credentials, try again')
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
 
 def FormsStudent(request):
     return render(request,"registerAsStudent.html")
